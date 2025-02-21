1afe3ed5ea7631c7ed6660626fb069398171360e
tungastreek <tung@astreek.com>
2025-02-21 14:59:47 +0700

    WIP Part 4 Using Joi for request validating

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -1,56 +1,91 @@
 const notesRouter = require('express').Router();
+const Joi = require('joi');
 const NoteModel = require('../models/note');
 
-notesRouter.get('/', (req, res) => {
-  NoteModel.find({}).then((notes) => res.json(notes));
+// Define the schema for validation
+const noteCreateSchema = Joi.object({
+  content: Joi.string().min(5).required(),
+  important: Joi.boolean().optional(),
 });
 
-notesRouter.post('/', (req, res, next) => {
-  const body = req.body;
-  const note = new NoteModel({
-    content: body.content,
-    important: body.important || false,
-  });
-
-  note
-    .save()
-    .then((savedNote) => res.json(savedNote))
-    .catch((err) => next(err));
+const noteUpdateSchema = Joi.object({
+  id: Joi.string().optional(),
+  content: Joi.string().min(5).required(),
+  important: Joi.boolean().optional(),
 });
 
-notesRouter.get('/:id', (req, res, next) => {
-  NoteModel.findById(req.params.id)
-    .then((note) => {
-      if (!note) {
-        return res.status(404).end();
-      }
-      res.json(note);
-    })
-    .catch((error) => next(error));
+// Get all notes
+notesRouter.get('/', async (req, res, next) => {
+  try {
+    const notes = await NoteModel.find({});
+    res.json(notes);
+  } catch (error) {
+    next(error);
+  }
 });
 
-notesRouter.put('/:id', (req, res, next) => {
-  NoteModel.findById(req.params.id)
-    .then((note) => {
-      if (!note) {
-        return res.status(404).end;
-      }
-      note.content = req.body.content;
-      note.important = req.body.important;
-      note
-        .save()
-        .then((savedNote) => {
-          res.json(savedNote);
-        })
-        .catch((error) => next(error));
-    })
-    .catch((error) => next(error));
+// Create a new note
+notesRouter.post('/', async (req, res, next) => {
+  const { error } = noteCreateSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  const { content, important = false } = req.body;
+  const note = new NoteModel({ content, important });
+
+  try {
+    const savedNote = await note.save();
+    res.json(savedNote);
+  } catch (error) {
+    next(error);
+  }
 });
 
-notesRouter.delete('/:id', (req, res, next) => {
-  NoteModel.findByIdAndDelete(req.params.id)
-    .then(() => res.status(204).end())
-    .catch((error) => next(error));
+// Get a note by ID
+notesRouter.get('/:id', async (req, res, next) => {
+  try {
+    const note = await NoteModel.findById(req.params.id);
+    if (!note) {
+      return res.status(404).end();
+    }
+    res.json(note);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Update a note by ID
+notesRouter.put('/:id', async (req, res, next) => {
+  const { error } = noteUpdateSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  try {
+    const note = await NoteModel.findById(req.params.id);
+    if (!note) {
+      return res.status(404).end();
+    }
+
+    note.content = req.body.content;
+    note.important = req.body.important;
+
+    const updatedNote = await note.save();
+    res.json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete a note by ID
+notesRouter.delete('/:id', async (req, res, next) => {
+  try {
+    await NoteModel.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = notesRouter;
